c3c62cf4d6dcc2e5df873ff3ec6e14c1173b4f65
lazy-chess-player <129977464+lazy-chess-player@users.noreply.github.com>
2024-04-15 03:42:24 +0800

    1

--- Problem_LeetCode/2023/Problem_230522_1/源.cpp
+++ Problem_LeetCode/2023/Problem_230522_1/源.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-struct TreeNode
+struct node
 {
     int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    node *left;
+    node *right;
+    node() : val(0), left(nullptr), right(nullptr) {}
+    node(int x) : val(x), left(nullptr), right(nullptr) {}
+    node(int x, node *left, node *right) : val(x), left(left), right(right) {}
 };
 class Solution
 {
 public:
-    TreeNode* sufficientSubset(TreeNode* root, int limit)
+    node *sufficientSubset(node *root, int limit)
     {
         limit -= root->val;
         if (root->left == root->right)
