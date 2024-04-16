e6ffd00413e3afb24e61b8db8129c6e08e797874
lazy-chess-player <129977464+lazy-chess-player@users.noreply.github.com>
2024-04-17 01:19:13 +0800

    1

---
+++ Problem_CodeForces/2024/0416/New_Year_Permutation.cpp
@@ -0,0 +1,271 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <list>
+#include <bitset>
+#include <cmath>
+#include <numeric>
+
+#define endl '\n'
+
+#define ft first
+#define sd second
+
+#define yes cout << "yes\n"
+#define no cout << "no\n"
+
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define unq_all(x) x.erase(unique(all(x)), x.end())
+#define sort_all(x) sort(all(x))
+#define reverse_all(x) reverse(all(x))
+
+#define range(x) for (int i = 0; i < (int)(x); i++)
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+
+#define RED cout << "\033[91m"
+#define GREEN cout << "\033[92m"
+#define YELLOW cout << "\033[93m"
+#define BLUE cout << "\033[94m"
+#define MAGENTA cout << "\033[95m"
+#define CYAN cout << "\033[96m"
+#define RESET cout << "\033[0m"
+
+// 红色
+#define DEBUG1(x)                     \
+    RED;                              \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 绿色
+#define DEBUG2(x)                     \
+    GREEN;                            \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 蓝色
+#define DEBUG3(x)                     \
+    BLUE;                             \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 品红
+#define DEBUG4(x)                     \
+    MAGENTA;                          \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 青色
+#define DEBUG5(x)                     \
+    CYAN;                             \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 黄色
+#define DEBUG6(x)                     \
+    YELLOW;                           \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+typedef pair<string, ll> psl;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pss> vpss;
+
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+typedef queue<int> qi;
+typedef queue<ll> ql;
+typedef queue<pii> qpii;
+typedef queue<pll> qpll;
+typedef queue<psi> qpsi;
+typedef queue<psl> qpsl;
+
+typedef priority_queue<int> pqi;
+typedef priority_queue<ll> pql;
+typedef priority_queue<string> pqs;
+typedef priority_queue<pii> pqpii;
+typedef priority_queue<psi> pqpsi;
+typedef priority_queue<pll> pqpl;
+typedef priority_queue<psi> pqpsl;
+
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, int> mci;
+typedef map<char, ll> mcl;
+typedef map<string, int> msi;
+typedef map<string, ll> msl;
+
+typedef unordered_map<int, int> umii;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<char, ll> umcl;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<string, ll> umsl;
+
+template <typename T>
+inline T read()
+{
+    T x = 0;
+    int y = 1;
+    char ch = getchar();
+    while (ch > '9' || ch < '0')
+    {
+        if (ch == '-')
+            y = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * y;
+}
+
+template <typename T>
+inline void write(T x)
+{
+    if (x < 0)
+    {
+        putchar('-');
+        x = -x;
+    }
+    if (x >= 10)
+    {
+        write(x / 10);
+    }
+    putchar(x % 10 + '0');
+}
+
+const int N = 310;
+int a[N];
+int p[N];
+bool vis[N];
+int n;
+
+// 返回x的祖宗节点
+int find(int x)
+{
+    // 压缩路径，将路径上所有节点赋值为集合编号
+    if (p[x] != x)
+        p[x] = find(p[x]);
+    return p[x];
+}
+
+// 初始化，假定节点编号是1~n
+void init()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        p[i] = i;
+    }
+}
+
+// 合并a和b所在的两个集合：
+void merge(int a, int b)
+{
+    int fa = find(a);
+    int fb = find(b);
+    if (fa == fb)
+        return;
+    p[fb] = fa;
+}
+
+void solve()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    init();
+    string s;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> s;
+        for (int j = 0; j < n; j++)
+        {
+            if (s[j] == '1')
+                merge(i, j + 1);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        int x = find(i);
+        if (vis[x])
+            continue;
+        vis[x] = true;
+        vi c;
+        vi d;
+        for (int j = i; j <= n; j++)
+        {
+            if (find(j) == x)
+            {
+                c.eb(a[j]);
+                d.eb(j);
+            }
+        }
+        sort_all(c);
+        for (int j = 0; j < sz(c); j++)
+        {
+            a[d[j]] = c[j];
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+    int _ = 1;
+    // std::cin >> _;
+    while (_--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+//
