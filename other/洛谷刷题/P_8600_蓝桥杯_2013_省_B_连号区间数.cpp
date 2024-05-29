c9e0240078d8ee84186cf8edc1ec83a30f083def
lazy-chess-player <129977464+lazy-chess-player@users.noreply.github.com>
2024-05-30 03:11:36 +0800

    1

---
+++ other/洛谷刷题/P_8600_蓝桥杯_2013_省_B_连号区间数.cpp
@@ -0,0 +1,225 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <list>
+#include <bitset>
+#include <cmath>
+#include <numeric>
+
+#define endl '\n'
+
+#define ft first
+#define sd second
+
+#define yes cout << "yes\n"
+#define no cout << "no\n"
+
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define pb push_back
+#define eb emplace_back
+
+#define all(x) x.begin(), x.end()
+#define unq_all(x) x.erase(unique(all(x)), x.end())
+#define sort_all(x) sort(all(x))
+#define reverse_all(x) reverse(all(x))
+
+#define INF 0x7fffffff
+#define INFLL 0x7fffffffffffffffLL
+
+#define RED cout << "\033[91m"
+#define GREEN cout << "\033[92m"
+#define YELLOW cout << "\033[93m"
+#define BLUE cout << "\033[94m"
+#define MAGENTA cout << "\033[95m"
+#define CYAN cout << "\033[96m"
+#define RESET cout << "\033[0m"
+
+// 红色
+#define DEBUG1(x)                     \
+    RED;                              \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 绿色
+#define DEBUG2(x)                     \
+    GREEN;                            \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 蓝色
+#define DEBUG3(x)                     \
+    BLUE;                             \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 品红
+#define DEBUG4(x)                     \
+    MAGENTA;                          \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 青色
+#define DEBUG5(x)                     \
+    CYAN;                             \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 黄色
+#define DEBUG6(x)                     \
+    YELLOW;                           \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pdd;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+typedef pair<string, ll> psl;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pss> vpss;
+
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+typedef queue<int> qi;
+typedef queue<ll> ql;
+typedef queue<pii> qpii;
+typedef queue<pll> qpll;
+typedef queue<psi> qpsi;
+typedef queue<psl> qpsl;
+
+typedef priority_queue<int> pqi;
+typedef priority_queue<ll> pql;
+typedef priority_queue<string> pqs;
+typedef priority_queue<pii> pqpii;
+typedef priority_queue<psi> pqpsi;
+typedef priority_queue<pll> pqpl;
+typedef priority_queue<psi> pqpsl;
+
+typedef map<int, int> mii;
+typedef map<int, bool> mib;
+typedef map<ll, ll> mll;
+typedef map<ll, bool> mlb;
+typedef map<char, int> mci;
+typedef map<char, ll> mcl;
+typedef map<char, bool> mcb;
+typedef map<string, int> msi;
+typedef map<string, ll> msl;
+typedef map<int, bool> mib;
+
+typedef unordered_map<int, int> umii;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<char, ll> umcl;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<string, ll> umsl;
+
+namespace FIO
+{
+    const short wr_bs = 10;
+    bool rd_f;
+    short rd_c, wr_s[1000005];
+    int wr_l = 0;
+    template <typename Ty>
+    inline void read(Ty &x)
+    {
+        for (rd_c = getchar(), rd_f = 0; rd_c < '0' || rd_c > '9'; rd_c = getchar())
+            if (rd_c == '-')
+                rd_f = 1;
+        for (x = 0; rd_c >= '0' && rd_c <= '9'; rd_c = getchar())
+            x = (x << 1) + (x << 3) + (rd_c ^ 48);
+        x = rd_f ? -x : x;
+    }
+    template <typename Ty>
+    inline void write(Ty x)
+    {
+        if (!x)
+            return (void)(putchar('0'));
+        if (x < 0)
+            putchar('-'), x = -x;
+        for (; x; x /= wr_bs)
+            wr_s[++wr_l] = x % wr_bs ^ 48;
+        for (; wr_l; wr_l--)
+            putchar(wr_s[wr_l]);
+    }
+}
+
+/*#####################################BEGIN#####################################*/
+
+const int N = 5e4 + 5;
+
+int a[N];
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+using namespace FIO;
+void solve()
+{
+    int n;
+    read(n);
+    for (int i = 1; i <= n; i++)
+    {
+        read(a[i]);
+    }
+    int cnt = 0;
+    for (int l = 1; l <= n - 1; l++)
+    {
+        int r = l + 1;
+        int mx = max(a[l], a[r]);
+        int mn = min(a[l], a[r]);
+        for (; r <= n; r++)
+        {
+            mx = max(mx, a[r]);
+            mn = min(mn, a[r]);
+            if (mx - mn == r - l)
+                cnt++;
+        }
+    }
+    int ans = n + cnt;
+    write(ans);
+    putchar('\n');
+}
+
+int main()
+{
+    ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+    int _ = 1;
+    // std::cin >> _;
+    while (_--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*######################################END######################################*/
+// 链接：
---
+++ other/洛谷刷题/P_8707_蓝桥杯_2020_省_AB_1_走方格.cpp
@@ -0,0 +1,215 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <list>
+#include <bitset>
+#include <cmath>
+#include <numeric>
+
+#define endl '\n'
+
+#define ft first
+#define sd second
+
+#define yes cout << "yes\n"
+#define no cout << "no\n"
+
+#define Yes cout << "Yes\n"
+#define No cout << "No\n"
+
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+
+#define pb push_back
+#define eb emplace_back
+
+#define all(x) x.begin(), x.end()
+#define unq_all(x) x.erase(unique(all(x)), x.end())
+#define sort_all(x) sort(all(x))
+#define reverse_all(x) reverse(all(x))
+
+#define INF 0x7fffffff
+#define INFLL 0x7fffffffffffffffLL
+
+#define RED cout << "\033[91m"
+#define GREEN cout << "\033[92m"
+#define YELLOW cout << "\033[93m"
+#define BLUE cout << "\033[94m"
+#define MAGENTA cout << "\033[95m"
+#define CYAN cout << "\033[96m"
+#define RESET cout << "\033[0m"
+
+// 红色
+#define DEBUG1(x)                     \
+    RED;                              \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 绿色
+#define DEBUG2(x)                     \
+    GREEN;                            \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 蓝色
+#define DEBUG3(x)                     \
+    BLUE;                             \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 品红
+#define DEBUG4(x)                     \
+    MAGENTA;                          \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 青色
+#define DEBUG5(x)                     \
+    CYAN;                             \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+// 黄色
+#define DEBUG6(x)                     \
+    YELLOW;                           \
+    cout << #x << " : " << x << endl; \
+    RESET;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pdd;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+typedef pair<string, ll> psl;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pss> vpss;
+
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+typedef queue<int> qi;
+typedef queue<ll> ql;
+typedef queue<pii> qpii;
+typedef queue<pll> qpll;
+typedef queue<psi> qpsi;
+typedef queue<psl> qpsl;
+
+typedef priority_queue<int> pqi;
+typedef priority_queue<ll> pql;
+typedef priority_queue<string> pqs;
+typedef priority_queue<pii> pqpii;
+typedef priority_queue<psi> pqpsi;
+typedef priority_queue<pll> pqpl;
+typedef priority_queue<psi> pqpsl;
+
+typedef map<int, int> mii;
+typedef map<int, bool> mib;
+typedef map<ll, ll> mll;
+typedef map<ll, bool> mlb;
+typedef map<char, int> mci;
+typedef map<char, ll> mcl;
+typedef map<char, bool> mcb;
+typedef map<string, int> msi;
+typedef map<string, ll> msl;
+typedef map<int, bool> mib;
+
+typedef unordered_map<int, int> umii;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<char, ll> umcl;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<string, ll> umsl;
+
+template <typename T>
+inline void read(T &x)
+{
+    T f = 1;
+    x = 0;
+    char ch = getchar();
+    while (0 == isdigit(ch))
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (0 != isdigit(ch))
+        x = (x << 1) + (x << 3) + ch - '0', ch = getchar();
+    x *= f;
+}
+
+template <typename T>
+inline void write(T x)
+{
+    if (x < 0)
+    {
+        x = ~(x - 1);
+        putchar('-');
+    }
+    if (x > 9)
+        write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+/*#####################################BEGIN#####################################*/
+
+const int N = 50;
+
+int dp[N][N];
+
+int n, m;
+void solve()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        dp[i][1] = 1;
+    for (int j = 1; j <= m; j++)
+        dp[1][j] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 2; j <= m; j++)
+        {
+            if (!(i & 1) && !(j & 1))
+                dp[i][j] = 0;
+            else
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+    cout << dp[n][m] << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+    int _ = 1;
+    // std::cin >> _;
+    while (_--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+/*######################################END######################################*/
+// 链接：
