11c402364bcf7a6ee3fd0718186dd09735e74d1f
lazy-chess-player <129977464+lazy-chess-player@users.noreply.github.com>
2024-10-11 22:01:05 +0800

    1011

--- Problem_CodeForces/2024/10/11/Points_on_Plane.cpp
+++ Problem_CodeForces/2024/10/11/Points_on_Plane.cpp
@@ -1,22 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <set>
-#include <unordered_map>
-#include <cstring>
-#include <cstdio>
-#include <string>
-#include <queue>
-#include <stack>
-#include <map>
-#include <list>
+#include <array>
 #include <bitset>
+#include <cassert>
 #include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <chrono>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
 #include <numeric>
-
-#define endl '\n'
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 #define ft first
 #define sd second
@@ -34,12 +43,16 @@
 #define eb emplace_back
 
 #define all(x) x.begin(), x.end()
+#define all1(x) x.begin() + 1, x.end()
 #define unq_all(x) x.erase(unique(all(x)), x.end())
+#define unq_all1(x) x.erase(unique(all1(x)), x.end())
 #define sort_all(x) sort(all(x))
+#define sort1_all(x) sort(all1(x))
 #define reverse_all(x) reverse(all(x))
+#define reverse1_all(x) reverse(all1(x))
 
-#define INF 0x7fffffff
-#define INFLL 0x7fffffffffffffffLL
+#define inf 0x3f3f3f3f
+#define infll 0x3f3f3f3f3f3f3f3fLL
 
 #define RED cout << "\033[91m"
 #define GREEN cout << "\033[92m"
@@ -90,13 +103,19 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
+// typedef __int128_t i128;
 
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef pair<ld, ld> pdd;
+typedef pair<ll, int> pli;
 typedef pair<string, string> pss;
 typedef pair<string, int> psi;
 typedef pair<string, ll> psl;
+
+typedef tuple<int, int, int> ti3;
+typedef tuple<ll, ll, ll> tl3;
+typedef tuple<ld, ld, ld> tld3;
 
 typedef vector<bool> vb;
 typedef vector<int> vi;
@@ -104,7 +123,11 @@ typedef vector<ll> vl;
 typedef vector<string> vs;
 typedef vector<pii> vpii;
 typedef vector<pll> vpll;
+typedef vector<pli> vpli;
 typedef vector<pss> vpss;
+typedef vector<ti3> vti3;
+typedef vector<tl3> vtl3;
+typedef vector<tld3> vtld3;
 
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
@@ -121,7 +144,7 @@ typedef priority_queue<ll> pql;
 typedef priority_queue<string> pqs;
 typedef priority_queue<pii> pqpii;
 typedef priority_queue<psi> pqpsi;
-typedef priority_queue<pll> pqpl;
+typedef priority_queue<pll> pqpll;
 typedef priority_queue<psi> pqpsl;
 
 typedef map<int, int> mii;
@@ -142,21 +165,26 @@ typedef unordered_map<char, ll> umcl;
 typedef unordered_map<string, int> umsi;
 typedef unordered_map<string, ll> umsl;
 
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
 template <typename T>
-inline void read(T &x)
+inline T read()
 {
-    T f = 1;
-    x = 0;
+    T x = 0;
+    int y = 1;
     char ch = getchar();
-    while (0 == isdigit(ch))
+    while (ch > '9' || ch < '0')
     {
         if (ch == '-')
-            f = -1;
+            y = -1;
         ch = getchar();
     }
-    while (0 != isdigit(ch))
-        x = (x << 1) + (x << 3) + ch - '0', ch = getchar();
-    x *= f;
+    while (ch >= '0' && ch <= '9')
+    {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * y;
 }
 
 template <typename T>
@@ -164,49 +192,59 @@ inline void write(T x)
 {
     if (x < 0)
     {
-        x = ~(x - 1);
         putchar('-');
+        x = -x;
     }
-    if (x > 9)
+    if (x >= 10)
+    {
         write(x / 10);
+    }
     putchar(x % 10 + '0');
 }
 
 /*#####################################BEGIN#####################################*/
+struct node
+{
+    int l, r;
+    int idx;
+    bool operator<(const node &rhs) const
+    {
+        if (l / idx == rhs.l / idx)
+        {
+            if ((l / idx) & 1)
+                return r < rhs.r;
+            return r > rhs.r;
+        }
+        return l < rhs.l;
+    }
+};
 
-const int N = 1e5 + 5;
-
-vi h[N];
+const int sq = 1e3;
 void solve()
 {
     int n;
     cin >> n;
-    int a, b;
-    for (int i = 1; i < n; i++)
+    vector<node> a(n);
+    for (int i = 0; i < n; i++)
     {
-        cin >> a >> b;
-        h[a].eb(b);
-        h[b].eb(a);
+        cin >> a[i].l >> a[i].r;
+        a[i].idx = i + 1;
     }
-    int ans = 0;
-    for (int i = 1; i <= n; i++)
+    sort(all(a), [&](node a, node b)
+         { return a.l / sq == b.l / sq ? ((a.l / sq & 1) ? a.r < b.r : a.r > b.r) : a.l < b.l; });
+    for (int i = 0; i < n; i++)
     {
-        int sum = 0;
-        for (auto u : h[i])
-        {
-            sum += h[u].size();
-        }
-        if (sum == n - 1)
-            ans++;
+        cout << a[i].idx << " \n"[i == n - 1];
     }
-    cout << ans << endl;
 }
 
 int main()
 {
-    ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+    ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
+    // freopen("test.in", "r", stdin);
+    // freopen("test.out", "w", stdout);
     int _ = 1;
-    std::cin >> _;
+    // std::cin >> _;
     while (_--)
     {
         solve();
